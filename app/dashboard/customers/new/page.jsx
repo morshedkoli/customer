d33a5ae605b77501029b835933a5017e09481aa7
MORSHED AL MAIN <murshedkoli@gmail.com>
2024-12-03 00:49:39 +0600

    update disable ssr

--- app/dashboard/customers/new/page.jsx
+++ app/dashboard/customers/new/page.jsx
@@ -2,13 +2,6 @@ import BreadcrumbDashboard from "@/components/dashboard/BreadcrumbDeshboard";
 import DashboardLayout from "@/components/layout/DashboardLayout";
 import RegisterForm from "@/components/RegisterForm";
 
-import dynamic from "next/dynamic";
-
-const DynamicComponentWithNoSSR = dynamic(
-  () => import("../components/BrowserOnlyComponent"),
-  { ssr: false }
-);
-
 export default function Page() {
   return (
     <DashboardLayout>
